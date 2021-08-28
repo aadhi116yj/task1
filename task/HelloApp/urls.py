8abfcc8fdbc89b3d8ef89042ecd1bc480f0e059c
aadhi116yj <aadhi.altruist@gmail.com>
2021-08-28 23:15:41 +0530

    update urls

--- task/HelloApp/urls.py
+++ task/HelloApp/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('show/', views.show, name="show"),
     path('update/<int:id>/', views.update, name="update"),
     path('delete/<int:id>/', views.delete, name="delete"),
+    path('delete/<int:id>/', views.delete, name="delete"),
+    path('delete/<int:id>/', views.delete, name="delete"),
+    path('delete/<int:id>/', views.delete, name="delete"),
+    path('register/', views.register, name="register"),
+    path('details/', views.details, name="details"),
+    path('replace/<int:id>/',views.replace, name="replace"),
+    path('dell/<int:id>/',views.dell, name="dell"),
 ]
